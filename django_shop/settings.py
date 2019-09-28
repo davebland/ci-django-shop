47dd963bba8d1849efe541c1cc53f8a4a17b7d02
Ubuntu <ubuntu@ip-172-31-24-16.eu-west-1.compute.internal>
2019-09-28 09:56:51 +0000

    added default value to C9_PID for non C9 environments

--- django_shop/settings.py
+++ django_shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_PID') + '.vfs.cloud9.eu-west-1.amazonaws.com']
+ALLOWED_HOSTS = [os.environ.get('C9_PID', '') + '.vfs.cloud9.eu-west-1.amazonaws.com']
 
 # Application definition
 
