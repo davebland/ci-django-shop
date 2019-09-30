b2cba2b070c0a2089d644e75bb76ea56010cb1c9
Ubuntu <ubuntu@ip-172-31-24-16.eu-west-1.compute.internal>
2019-09-30 18:49:35 +0000

    setup for heroku

--- django_shop/settings.py
+++ django_shop/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_PID', '') + '.vfs.cloud9.eu-west-1.amazonaws.com']
+ALLOWED_HOSTS = [os.environ.get('C9_PID', '') + '.vfs.cloud9.eu-west-1.amazonaws.com', 'the-django-shop.herokuapp.com']
 
 # Application definition
 
