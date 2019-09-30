a210bea61195ad40a84e8be0a76831bd7ab923b9
Ubuntu <ubuntu@ip-172-31-24-16.eu-west-1.compute.internal>
2019-09-30 18:32:40 +0000

    fixed media serve from S3 issue

--- django_shop/settings.py
+++ django_shop/settings.py
@@ -157,10 +157,10 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
